576da3f9dd28edf158bfe02326f57f931c44aac8
rotlesnake <rotlesnake@yandex.ru>
2020-09-24 16:54:45 +0300

    latest

--- admin_development/backend/Auth/Auth.php
+++ admin_development/backend/Auth/Auth.php
@@ -9,26 +9,52 @@ class Auth
 {
 
 	public $user = null;
+	public $login    = "admin";
+	public $password = "12345";
+	public $token = "-";
 
 	
 	public function __construct(){
-
+            $this->token = sha1($this->login.$this->password);
 	}
 	
 
         public function isGuest() {
           if (!isset($this->user)) return true;
+          return false;
         }
+
+
+
+        public function autoLogin($request) {
+           if ($this->isGuest() && $request->hasHeader('Authorization')) {
+               $token = $request->getHeader('Authorization');
+               $this->login(["token"=>$token]);
+           }
+           if ($this->isGuest() && $request->hasHeader('token')) {
+               $token = $request->getHeader('token');
+               $this->login(["token"=>$token]);
+           }
+           if ($this->isGuest() && $request->hasCookie('token')) {
+               $token = $request->getCookie('token');
+               $this->login(["token"=>$token]);
+           }
+        }
+
 
 
         public function login($credentials) {
             $APP = App::getInstance();
-            if ($credentials['login']=="admin" && $credentials['password']=="12345") {
+            if (isset($credentials['login']) && $credentials['login']==$this->login && $credentials['password']==$this->password) {
                 $hours_token = 4;
                 $this->user = new EmptyClass();
-                $this->user->token_expire = date("Y-m-d H:i:s", strtotime("+".$hours_token." hours"));
-                $this->user->token = sha1($credentials['login'].$this->user->token_expire);
+                $this->user->token = $this->token;
                 setcookie("token", $this->user->token, time()+($hours_token*60*60), $APP->ROOT_URL, $_SERVER["SERVER_NAME"]);
+                return true;
+            }
+            if (isset($credentials['token']) && $credentials['token']==$this->token) {
+                $this->user = new EmptyClass();
+                $this->user->token = $this->token;
                 return true;
             }
             return false;
