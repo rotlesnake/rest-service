463e0780dde119fba0e62a28a474a6cd34a726b3
alexey <rotlesnake@yandex.ru>
2020-12-21 16:41:34 +0300

    second commit

--- admin_development/backend/Migrate/Controllers/MigrateController.php
+++ admin_development/backend/Migrate/Controllers/MigrateController.php
@@ -8,6 +8,9 @@ class MigrateController extends \MapDapRest\Controller
 
     public function indexAction($request, $response, $params) {
 
+       $APP = \MapDapRest\App::getInstance();
+       $APP->APP_PATH = ROOT_APP_PATH;
+
        $result = \MapDapRest\Migrate::migrate();
 
        $response->setError(0, "OK");
