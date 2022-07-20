5facf6780a8afdf5b66c2afcc5e00ee9417ffd88
alexey <rotlesnake@yandex.ru>
2022-07-20 10:53:35 +0300

    updates

--- App/Auth/Controllers/LoginController.php
+++ App/Auth/Controllers/LoginController.php
@@ -6,24 +6,33 @@ class LoginController extends \MapDapRest\Controller
 {
 
     public $APP;
+    public $requireAuth = false;
 
 
     public function __construct($app, $request, $response, $args)
     {
         $this->APP = $app;
-        if ($request->hasParam("login")) {
-           $data = ["login"=>$request->params["login"], "password"=>$request->params["password"]];
-           $this->APP->auth->login($data);
+        if ($request->hasParam("login") && $request->hasParam("password")) {
+            $data = ["login"=>$request->params["login"], "password"=>$request->params["password"]];
+            $this->APP->auth->login($data);
+            return;
+        }
+        if ($request->hasParam("token")) {
+            $data = ["token"=>$request->params["token"]];
+            $this->APP->auth->login($data);
+            return;
         }
     }
 
 
-    /** Авторизация пользователя <br>
-       @login <br>
-       @password <br>
-       @return [user:{}]
-    **/
+    /** Авторизация пользователя **/
     public function indexAction($request, $response, $params) {
+       if ($this->APP->auth->isGuest()) {
+           $response->setResponseCode(401);
+           return ["error"=>1, "message"=>"Ошибка в логине или пароле"];
+           die();
+       }
+
        return ["user"=>$this->APP->auth->getFields()];
     }
 
